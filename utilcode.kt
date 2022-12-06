005bdcb7c412d6d8554645a13c457ea647aefe6a
Winecran <bruno.denis@gmail.com>
2022-12-06 14:17:54 +0100

    Update utilcode.kt

--- utilcode.kt
+++ utilcode.kt
@@ -20,7 +20,7 @@ private fun readMutableMap() = readLn().groupingBy { it }.eachCount().toMutableM
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 class MutableStack<E>(vararg items: E) {
-    val elements = items.toMutableList()
+    private val elements = items.toMutableList()
     fun push(element: E) = elements.add(element)
     fun peek(): E = elements.last()
     fun pop(): E = elements.removeAt(elements.size - 1)
